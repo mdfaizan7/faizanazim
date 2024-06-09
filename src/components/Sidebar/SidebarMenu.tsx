b4546c8aead0f74a8530ceb3bee69322d612441a
mdfaizan7 <faizaanazim@gmail.com>
2024-06-09 12:10:18 +0530

    update

--- src/components/Sidebar/SidebarMenu.tsx
+++ src/components/Sidebar/SidebarMenu.tsx
@@ -9,15 +9,18 @@ interface Props {
 }
 
 const SidebarMenu = (props: Props) => {
-  const hoverClass = !props.selected ? "group-hover:" : "";
+  const { selected } = props;
 
   return (
-    <Link className={"group flex items-center py-3"} href={`#${props.hash}`}>
+    <Link
+      className="group flex items-center py-3 active"
+      href={`#${props.hash}`}
+    >
       <span
-        className={`nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all ${hoverClass}w-16 ${hoverClass}bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none`}
+        className={`nav-indicator mr-4 h-px ${selected ? "w-16" : "w-8"} ${selected ? "bg-slate-200" : "bg-slate-500"} transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none`}
       />
       <span
-        className={`nav-text text-xs ${hoverClass ? "" : "font-bold"} uppercase tracking-widest ${hoverClass ? "text-slate-500" : ""} ${hoverClass}text-slate-200 group-focus-visible:text-slate-200`}
+        className={`nav-text text-xs ${selected ? "font-bold" : ""} uppercase tracking-widest text-slate-${selected ? "200" : "500"} group-hover:text-slate-200 group-focus-visible:text-slate-200`}
       >
         {props.label}
       </span>
