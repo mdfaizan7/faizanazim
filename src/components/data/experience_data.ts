126ef71e2eafa0d6c60e9cb924136a469e3b659b
mdfaizan7 <faizaanazim@gmail.com>
2024-06-21 18:23:56 +0530

    update experience data

--- src/components/data/experience_data.ts
+++ src/components/data/experience_data.ts
@@ -5,14 +5,14 @@ export const EXPERIENCE_DATA_LIST: ExperienceCardProps[] = [
     companyHomePage: "https://developers.google.com/",
     companyName: "Google",
     description: `Just started at Google. More details coming soon!`,
-    duration: "Jun 2020 - Present",
+    duration: "Jun 2024 - Present",
     position: "Software Engineer III"
   },
   {
     companyHomePage: "https://www.getpowerplay.in/",
     companyName: "Powerplay",
     description: `Introduced TypeScript to Powerplay WebApp for enhanced productivity. Implemented dynamic backend-driven views and paginated tabular rendering with inline editing. Optimized Task Grid for faster performance and keyboard interactions. Improved search functionality and critical backend API performance. Led code quality discussions and enforced guidelines. Developed Kafka-based data streaming system. Created admin dashboard for faster ticket resolution. Assumed on-call duties and mentored junior team members.`,
-    duration: "Nov 2020 - Jun 2020",
+    duration: "Nov 2020 - Jun 2024",
     position: "Senior Software Engineer"
   },
   {
